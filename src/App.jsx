a06c06b2a0816a60a3ad7abb2ca10f6fff76ed76
xtwilightx <iqxidi@gmail.com>
2025-07-08 16:28:56 +0300

    Add header (logo, nav text), add site description and button Get in touch

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,10 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Logo from './icons/Logo.svg';
+import Logo from './assets/icons/Logo.svg';
+
+import manLookingAtTheCamera from './assets/images/man-looking-at-camera.png'
+import bgForManLookingATC from './assets/images/bg-for-man-looking-at-camera.jpg'
 function App() {
   const [count, setCount] = useState(0)
 
@@ -22,14 +25,26 @@ function App() {
 
     </header>
 
+
+  <div className='flex'>
     <div className="w-full">
-        <h2 className="text-[60px]">Hey, I’m Nick</h2>
+        <h2 className="text-[60px] font-[Montserrat]">Hey, I’m Nick</h2>
         <h3 className='text-[20px]'>Frontend developer</h3>
+
   <h3>Help you to create high-quality digital</h3>
   <h3>products that your clients will love and let</h3>
   <h3>your business thrive</h3>
     </div>
 
+  <div className='relative'>
+  <img className='rounded-[100px] rotate-250 w-[1478px] h-[864px]' src={bgForManLookingATC} />
+  <div className='absolute'>
+  <img className='w-[1070px] h-[689px]' src={manLookingAtTheCamera} />
+  </div>
+  </div>
+  </div>
+
+  <button className='text-white w-[188px] h-[50px] bg-[#7E3AF2] py-2 px-4 rounded-full'>Get in touch</button>
     </>
   )
 }
