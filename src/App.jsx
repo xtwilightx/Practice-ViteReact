835ee3c1b1193daf84cb7a16ab550dcc793bb075
xtwilightx <iqxidi@gmail.com>
2025-07-09 18:03:37 +0300

    add first section on mobile

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,13 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Logo from './assets/icons/Logo.svg';
+import BurgerMenu from './assets/icons/BurgerMenu.svg';
+import Plane from './assets/icons/Plane.svg';
 
-import manLookingAtTheCamera from './assets/images/man-looking-at-camera.png'
-import bgForManLookingAtTheCamera from './assets/images/bg-for-man-looking-at-camera.jpg'
-import ManRaisingUpLookingAtTheCamera from './assets/images/portrait-of-young-handsome-man-in-jean-shirt-raising-up-brow-looking-at-camera.png'
+import manLookingAtTheCamera from './assets/images/man-looking-at-camera.png';
+import bgForManLookingAtTheCamera from './assets/images/bg.jpg';
+import bgMobile from './assets/images/bgMobile.png';
+import ManInCircle from './assets/images/ManInCircle.png';
 import { Button } from './ui/Button';
 function App() {
   const [count, setCount] = useState(0)
@@ -14,10 +17,12 @@ function App() {
   return (
     <>
       <header>
+        <div className=''></div>
         <div className="mb-[83px] flex items-center justify-between w-full h-[64px] bg-[#FCFBFA]">
           <div className='flex'><img className='flex-none' src={Logo} alt="React Logo" /></div>
 
-          <div className='flex gap-20'>
+          <div className='flex-col'>
+
             <a className='flex-initial'>Main</a>
             <a className='flex-initial'>About</a>
             <a className='flex-initial'>Get in touch</a>
@@ -28,31 +33,29 @@ function App() {
       </header>
 
 
-      <div className=''>
-        <div className="">
-          <h2 className="text-[60px] font-[Montserrat]">Hey, I’m Nick</h2>
-          <h3 className='text-[20px]'>Frontend developer</h3>
-
-          <h3>Help you to create high-quality digital</h3>
-          <h3>products that your clients will love and let</h3>
-          <h3>your business thrive</h3>
-          <button className='text-white w-[188px] h-[50px] bg-[#7E3AF2] py-2 px-4 rounded-full'>Get in touch</button>
+      <div className='relative w-full h-[300px] overflow-hidden'>
+        <div className="flex flex-col gap-3 w-2/3">
+          <h2 className="text-[24px] font-bold font-[Montserrat]">Hey, I’m Nick</h2>
+          <div className='text-[14px] w-full'>Frontend developer Help you to create high-quality digital products that your clients will love and let your business thrive
+            </div>
+          <div>
+          <Button text={"Get in touch"}/>
+          </div>
         </div>
 
-        <div className='hidden'>
-          <img className='rounded-4xl rotate-200 w-[1478px] h-[864px]' src={bgForManLookingAtTheCamera} />
-          <div className='absolute left-0 bottom-0'>
-            <img className='w-[1070px] h-[689px]' src={manLookingAtTheCamera} />
-          </div>
+
+        <div className='-z-10 absolute right-0 bottom-0'>
+          <img className='rounded-4xl sm:hidden' src={bgMobile} />
+          {/* <div className='absolute left-0 bottom-0'>
+            <img className='' src={manLookingAtTheCamera} />
+          </div> */}
         </div>
       </div>
 
-      <div className='flex'>
+      <div className='flex-col'>
 
-        <div className=''>About me</div>
-        <img className='w-[364px] h-[364px] rounded-full object-cover' src={ManRaisingUpLookingAtTheCamera} />
-
-
+        <h2 className="text-[24px] font-bold font-[Montserrat]">About me</h2>
+        <img className='w-[364px] h-[364px] rounded-full object-cover' src={ManInCircle} />
 
       </div>
     </>
--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -1,1 +1,5 @@
-export const Button = ({text}) => <button className="flex items-center p-5 bg-violet-600">{text} samolet</button>
+import Plane from '../assets/icons/Plane.svg';
+export const Button = ({text}) => <button className='text-[14px]  flex gap-2 text-white bg-[#7E3AF2] py-2 px-4 rounded-full'>
+<span>{text}</span>
+<img className='' src={Plane} alt="React Logo" />
+</button>
