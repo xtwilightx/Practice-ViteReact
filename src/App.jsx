cce483d170485e1f9667420e4d74199c0d1f6f4f
xtwilightx <iqxidi@gmail.com>
2025-07-18 08:36:26 +0300

    change size button component

--- src/App.jsx
+++ src/App.jsx
@@ -44,39 +44,39 @@ function App() {
       </header>
 
       <main>
-  <div className='pl-4 sm:pl-10 lg:pl-20'>
-        <div className='mb-[48px] relative w-full h-[440px]'>
-          <div className="flex flex-col gap-3 w-2/3">
-            <h1 className="text-[24px] font-bold text-[#27272A] hidden sm:block sm:text-6xl lg:text-6xl">Hey, I’m Nick</h1>
-            <h3 className="text-2xl font-bold text-[#27272A] sm:hidden">Hey, I’m Nick</h3>
-            <span className='text-[14px] font-normal w-full text-[#696969] mb-[24px] sm:text-2xl'>Frontend developer
-            </span>
-            <hr className='mb-[24px] w-9 border-2 text-[#7E3AF2]'></hr>
-            <span className='mb-[24px] text-[14px] font-normal w-full text-[#696969] sm:text-2xl'>Help you to create high-quality digital products that your clients will love and let your business thrive
-            </span>
-            <div>
-              <Button text={"Get in touch"} />
+        <div className='pl-4 sm:pl-10 lg:pl-20'>
+          <div className='mb-[48px] relative w-full h-[440px]'>
+            <div className="flex flex-col gap-3 w-2/3">
+              <h1 className="text-[24px] font-bold text-[#27272A] hidden sm:block sm:text-6xl lg:text-6xl">Hey, I’m Nick</h1>
+              <h3 className="text-2xl font-bold text-[#27272A] sm:hidden">Hey, I’m Nick</h3>
+              <span className='text-[14px] font-normal w-full text-[#696969] mb-[24px] sm:text-2xl'>Frontend developer
+              </span>
+              <hr className='mb-[24px] w-9 border-2 text-[#7E3AF2]'></hr>
+              <span className='mb-[24px] text-[14px] font-normal w-full text-[#696969] sm:text-2xl'>Help you to create high-quality digital products that your clients will love and let your business thrive
+              </span>
+              <div>
+                <Button text={"Get in touch"} />
+              </div>
+            </div>
+
+
+            <div className='-z-10 absolute right-0 top-[207px] sm:bottom-21'>
+              <img className='rounded-l-4xl sm:hidden' src={bgMobile} />
+              <img className='rounded-l-4xl hidden sm:block sm:h-[360px]' src={bgTablet} />
             </div>
           </div>
 
 
-          <div className='-z-10 absolute right-0 bottom-0 sm:bottom-21'>
-            <img className='rounded-l-4xl sm:hidden' src={bgMobile} />
-            <img className='rounded-l-4xl hidden sm:block sm:h-[360px]' src={bgTablet} />
-          </div>
-        </div>
-
-
-          <section>
-          <div className='sm:flex flex-row gap-11'>
-            <div className='sm:w-[calc(40%-1.5rem)]'>
-            <h2 className="mb-[36px] text-2xl sm:text-3xl sm:font-bold font-semibold text-[#27272A]">About me</h2>
-            <img className='pl-0 mb-[36px] w-full px-4 rounded-full object-cover' src={ManInCircle} />
+          <section className='pt-[55px]'>
+            <div className='sm:flex flex-row gap-11'>
+              <div className='sm:w-[calc(40%-1.5rem)]'>
+              <h2 className="mb-[36px] text-2xl sm:text-3xl sm:font-bold font-semibold text-[#27272A]">About me</h2>
+              <img className='pl-0 mb-[36px] w-full px-4 rounded-full object-cover' src={ManInCircle} />
             </div>
             
             <div className=''>
-            <hr className='mb-[24px] w-9 border-2 text-[#7E3AF2]'></hr>
-            <p className='mb-[48px] text-[14px] sm:text-2xl font-normal text-[#696969]'><span className='font-semibold text-[#000000]'>Nick Richardson</span> - specialist in Frontend development. Clear code is my passion.<br /> Solving issues through negotiations </p>
+              <hr className='mb-[24px] w-9 border-2 text-[#7E3AF2]'></hr>
+              <p className='mb-[48px] text-[14px] sm:text-2xl font-normal text-[#696969]'><span className='font-semibold text-[#000000]'>Nick Richardson</span> - specialist in Frontend development. Clear code is my passion.<br /> Solving issues through negotiations </p>
               {/* <Button text={"Get in touch"} /> */}
             </div>
             </div>
